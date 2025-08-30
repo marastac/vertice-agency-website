e6b3f17de68ed05d99ca4ff12344a4b20d7f1492
marastac <132803403+marastac@users.noreply.github.com>
2025-08-30 01:29:03 -0500

    chore(core): activa reporte de Web Vitals

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { reportWebVitals } from './utils/webvitals'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+// Core Web Vitals -> GA4/Pixel (seguro, no bloquea render)
+reportWebVitals()
