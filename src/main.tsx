62f867d2445548424a0716935ef3f59902440b6f
marastac <132803403+marastac@users.noreply.github.com>
2025-09-02 01:43:29 -0500

    chore: inicializa Web Vitals correctamente

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { reportWebVitals } from './utils/webvitals'
+// src/main.tsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { initWebVitals } from './utils/webvitals';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
 
 // Core Web Vitals -> GA4/Pixel (seguro, no bloquea render)
-reportWebVitals()
+if ('requestIdleCallback' in window) {
+  (window as any).requestIdleCallback(() => initWebVitals());
+} else {
+  setTimeout(() => initWebVitals(), 1);
+}
